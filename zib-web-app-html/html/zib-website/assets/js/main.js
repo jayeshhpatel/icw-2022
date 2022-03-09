1779437a04661f5b3f6d883c33f98f45a7afb386
Jayesh Patel <send2jayesh@gmail.com>
2022-03-09 09:49:52 +0530

    Update ZIB

---
+++ zib-web-app-html/html/zib-website/assets/js/main.js
@@ -0,0 +1,28 @@
+/*-----------------------------------------------------------------------------------*/
+/* MAIN
+/*-----------------------------------------------------------------------------------*/
+$(document).ready(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+ 
+    $('.toggle-sidebar,.bg-overly').on('click', function (e) {
+        $('.bg-overly,.toggle-sidebar,body,.main-header').toggleClass('is-visible');
+        e.preventDefault();
+    });
+    $('.filter-btn').on('click', function (e) {
+        $('.filter-dropdown').toggleClass('is-open');
+        e.preventDefault();
+    });
+
+    if ($('.main-header').length) {
+        $(window).scroll(function () {
+            if (jQuery(this).scrollTop() > 10) {
+                $(".main-header").addClass("fixed-header");
+            } else {
+                $(".main-header").removeClass("fixed-header");
+            }
+        });
+    }
+  
+    new WOW().init();
+});
+
